e8824486133bb205657a33025d435fb35bbb7147
morsals1 <157448960+morsals1@users.noreply.github.com>
2025-02-17 10:28:04 +0500

    Add files via upload

---
+++ Lab_Process13-main/Lab_Process13-main/LabProcess13/LabProcess13.cpp
@@ -0,0 +1,67 @@
+﻿#include <windows.h>
+#include <conio.h>
+#include <iostream>
+
+int main() {
+
+
+    STARTUPINFO si = { sizeof(si) };
+    PROCESS_INFORMATION pi;
+
+
+    wchar_t choice[100];
+    BOOL fConnected;
+    HANDLE hPipe;
+    DWORD  cbRead;
+    DWORD  cbWritten;
+
+    wchar_t cmdline[] = L"C:\\Users\\st310-11\\Desktop\\меньшиков ПР-32\\Lab_Process13-main\\Lab_Process13child\\Lab13Child\\x64\\Debug\\Lab13Child.exe";
+
+    hPipe = CreateNamedPipe(cmdline, PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT, PIPE_UNLIMITED_INSTANCES,
+        1024, 1024, 5000, NULL);
+
+    if (hPipe == NULL) {
+        std::cerr << "Ошибка создания pipe: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    fConnected = ConnectNamedPipe(hPipe, NULL);
+
+    while (true) {
+        std::cout << "1. Создать новый поток\n";
+        std::cout << "2. Выйти\n";
+        std::cout << "Выберите действие: ";
+
+        _getws_s(choice);
+        _getch();
+
+        if (choice == L"1") {
+            if (!WriteFile(hPipe, choice, wcslen(choice) + 1, &cbWritten, NULL))
+                break;
+        }
+        else if (choice == L"2") {
+            if (!WriteFile(hPipe, choice, wcslen(choice) + 1, &cbWritten, NULL))
+                break;
+        }
+        else {
+            std::cout << "Неверный выбор.\n";
+        }
+    }
+
+    if (!CreateProcess(NULL, cmdline, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
+        std::cerr << "Ошибка создания процесса: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    std::cout << "процесс создан\n";
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    std::cout << "процесс завершен. Нажмите любую клавишу для выхода.\n";
+    _getch();
+
+    return 0;
+}
---
+++ Lab_Process13-main/Lab_Process13child/Lab13Child/Lab13Child/Lab13Child.cpp
@@ -0,0 +1,72 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <conio.h>
+#include <vector>
+
+
+DWORD WINAPI ThreadFunc(LPVOID param) {
+    int id = (int)(uintptr_t)param;
+    std::cout << "Поток " << id << " начал выполнение.\n";
+
+    srand(static_cast<unsigned int>(time(nullptr)) + id);
+
+    while (true) {
+        int randomNumber = rand();
+        std::cout << "Поток " << id << " сгенерировал число: " << randomNumber << "\n";
+
+        Sleep(1000);
+    }
+
+    std::cout << "Поток " << id << " завершил выполнение.\n";
+    return 0;
+}
+
+int main() {
+
+    std::vector<HANDLE> threads;
+    int nextThreadId = 1;
+    wchar_t cmdline[] = L"C:\\Users\\st310-11\\Desktop\\меньшиков ПР-32\\Lab_Process13-main\\Lab_Process13-main\\LabProcess13\\x64\\Debug\\LabProcess13.exe";
+
+    HANDLE hPipe;
+    DWORD  cbWritten;
+    DWORD  cbRead;
+    wchar_t choice[100];
+
+    hPipe = CreateFile(cmdline, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+
+    if (hPipe == NULL) {
+        std::cerr << "Ошибка создания pipe: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    while (true) {
+        std::cout << choice ;
+
+        if (ReadFile(hPipe, choice, 1024, &cbRead, NULL)) {
+            HANDLE hThread = CreateThread(NULL, 0, ThreadFunc, (LPVOID)(uintptr_t)nextThreadId++, 0, NULL);
+            if (hThread == NULL) {
+                std::cerr << "Ошибка создания потока\n";
+            }
+            else {
+                threads.push_back(hThread);
+                std::cout << "Поток " << nextThreadId - 1 << " создан.\n";
+            }
+        }
+        else if (ReadFile(hPipe, choice, 1024, &cbRead, NULL)) {
+            break;
+        }
+        else {
+            std::cout << "Неверный выбор.\n";
+        }
+    }
+
+    for (size_t i = 0; i < threads.size(); ++i) {
+        TerminateThread(threads[i], 0);
+        CloseHandle(threads[i]);
+    }
+
+    std::cout << "Все потоки завершены. Нажмите любую клавишу для выхода.\n";
+    _getch();
+
+    return 0;
+}
